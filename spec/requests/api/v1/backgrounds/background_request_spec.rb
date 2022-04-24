1f72b3e24ff0c17e1a7a869e553d57ec2b54bd77
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-24 16:58:30 -0600

    Add: request spec files

---
+++ spec/requests/api/v1/backgrounds/background_request_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'Background Request', type: :request do
+  before do
+
+  end
+
+end
---
+++ spec/requests/api/v1/forecasts/forecast_request_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Request', type: :request do
+  before do
+
+  end
+
+end
