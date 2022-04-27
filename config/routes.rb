213e7468dec5927faae4ba2b6f65d3a1294c2cca
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-27 07:32:37 -0600

    Update: namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
 
-  # namespace :api do
-  #   namespace :v1 do
-  #     # resources :backgrounds, only: :show
-  #     get 'backgrounds', to: 'backgrounds#show'
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      get 'backgrounds', to: 'backgrounds#show'
+      get 'forecast', to: 'forecasts#show'
+      get 'munchies', to: 'munchies#show'
 
-  get 'api/v1/backgrounds', to: 'backgrounds#show'
-  get 'api/v1/forecast', to: 'forecast#show'
-  get 'api/v1/munchies', to: 'munchies#show'
+      post 'users', to: 'users#create'
+      post 'sessions', to: 'users#login'
 
-  post 'api/v1/road_trip', to: 'road_trips#show'
-
-  post 'api/v1/users', to: 'users#create'
-  post 'api/v1/sessions', to: 'users#login'
+      post 'road_trip', to: 'road_trips#show'
+    end
+  end
 end
